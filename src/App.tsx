e55ae76523952f380800e1253874a1f075a659fe
Julia <rina.julianur@gmail.com>
2026-07-09 14:27:38 +0700

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+    return (
+        <div>
+            <header>
+
+            </header>
+            <main>
+
+            </main>
+            <footer>
+
+            </footer>
+        </div>
+    );
+}
+
+export default App;
